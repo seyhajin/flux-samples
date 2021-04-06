f271f0261ae718fc5c5eb7713266d1a6e2d640e2
seyhajin <seyhajin@gmail.com>
2021-04-06 09:03:05 +0200

    First sources commit

---
+++ hello/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <flux-mods/hello/hello.h>
+
+int main(int argc, char* argv[]) {
+    print_hello();
+    return 0;
+}
---
+++ sdl2/hello-sdl/main.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <mods/sx/sx.h>
+#include <mods/sdl2/SDL/include/SDL.h>
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+int sx_main(void) {
+    SDL_Window *window = NULL;
+    SDL_Surface *surface = NULL;
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        fprintf(stderr, "could not initialize sdl2: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    window = SDL_CreateWindow(
+        "hello sdl2",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        SCREEN_WIDTH, SCREEN_HEIGHT,
+        SDL_WINDOW_SHOWN|SDL_WINDOW_RESIZABLE|SDL_WINDOW_ALLOW_HIGHDPI
+    );
+
+    if (window == NULL) {
+        fprintf(stderr, "could not create window: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    surface = SDL_GetWindowSurface(window);
+    SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0x1E, 0x1E, 0x32));
+    SDL_UpdateWindowSurface(window);
+
+    SDL_bool running = SDL_TRUE;
+    while(running) {
+        SDL_Event event;
+        while(SDL_PollEvent(&event)) {
+            switch(event.type) {
+            case SDL_WINDOWEVENT:
+                switch(event.window.event) {
+                case SDL_WINDOWEVENT_CLOSE:
+                    running = SDL_FALSE;
+                    break;
+                }
+                break;
+            }
+        }
+
+        //--* code here *--//
+    }
+    
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    
+    return 0;
+}
